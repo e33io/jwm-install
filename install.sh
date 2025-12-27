f818bf8a9554113388f1409d32675c63d40a5043
e33io <info@e33.io>
2025-12-27 00:22:46 -0600

    Update formatting

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # clone scripts repo
-git clone https://github.com/e33io/scripts "$HOME"/scripts
+git clone https://github.com/e33io/scripts ~/scripts
 
 # run distro specific jwm install script
-if [ -f "/etc/pacman.conf" ]; then
-    sh "$HOME"/scripts/arch-install-jwm.sh
-elif [ -f "/etc/debian_version" ]; then
-    sh "$HOME"/scripts/debian-install-jwm.sh
+if [ -f /etc/pacman.conf ]; then
+    sh ~/scripts/arch-install-jwm.sh
+elif [ -f /etc/debian_version ]; then
+    sh ~/scripts/debian-install-jwm.sh
 fi
 
 # clean up user directory
-if [ -f "$HOME/.install-info" ]; then
-    rm -rf "$HOME"/jwm-install
+if [ -f ~/.install-info ]; then
+    rm -rf ~/jwm-install
 fi
